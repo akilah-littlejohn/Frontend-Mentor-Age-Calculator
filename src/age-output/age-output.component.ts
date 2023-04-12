81e9b19eca1f26fe599126ef65fa7da70efd188c
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-04-12 09:19:04 -0500

    seperate AppComponent template logic into seperate components

---
+++ src/age-output/age-output.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-age-output',
+  templateUrl: './age-output.component.html',
+  styleUrls: ['./age-output.component.css'],
+  standalone:true
+})
+export class AgeOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,20 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { FormComponent } from './form/form.component';
+import { AgeOutputComponent } from './age-output/age-output.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormComponent, AgeOutputComponent],
   template: `
-  <h1>Age Calculator</h1>
+  <h1 class="title">Age Calculator</h1>
 
   <section>
-  <form action="">
-    <fieldset class="fieldset">
-      <label for="">Day</label>
-      <input type="number" name="" id="">
-    </fieldset>
-
-    <fieldset>
-      <label for="">Month</label>
-      <input type="number" name="" id="">
-    </fieldset>
-
-    <fieldset>
-      <label for="">Years</label>
-      <input type="number" name="" id="">
-    </fieldset>
-
-  </form>
+<app-form></app-form>
   <hr>
-  <h1 class="output"><span class="date">38</span>Years</h1>
-  <h1 class="output"><span class="date">3</span> months</h1>
-  <h1 class="output" > <span class="date">26</span> days</h1>
+<app-age-output></app-age-output>
 </section>
   `,
 })
