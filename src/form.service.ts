30e66bb3d218bd292ba2c611894438171c61663b
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-03 15:23:45 -0500

    2 files modified, 1 added

---
+++ src/form.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Injectable()
+export class FormService {
+  #fb: FormBuilder;
+  form: FormGroup;
+
+  ageValidation() {
+    this.form = this.#fb.group({
+      day: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+      month: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+      year: [
+        '',
+        [Validators.required, Validators.min(1900), this.futureYearValidator],
+      ],
+    });
+  }
+
+  futureYearValidator(control) {
+    const year = Number(control.value);
+    const currentYear = new Date().getFullYear();
+    if (year > currentYear) {
+      return { futureYear: true };
+    }
+    return null;
+  }
+
+  dateValidator(group) {
+    const day = Number(group.get('day').value);
+    const month = Number(group.get('month').value);
+    const year = Number(group.get('year').value);
+
+    if (isNaN(day) || isNaN(month) || isNaN(year)) {
+      return null;
+    }
+
+    const date = new Date(year, month - 1, day);
+    const isValid = date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
+
+    return isValid ? null : { invalidDate: true };
+  }
+}
